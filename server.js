567b123496a802e9b51e396b3a7a7da16d4f2c26
Rizvi Rahman Omee <rizvirahman.se@gmail.com>
2025-10-27 19:13:00 +0600

    initialised routes

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config(); 
+
+const userRoutes = require('./routes/userRoutes');  
+const orderRoutes = require('./routes/orderRoutes');  
+const itemRoutes = require('./routes/itemRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+
+// routes
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/items', itemRoutes);
+
+// Connect to MongoDB and start the server
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}\nhttp://localhost:${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('Failed to connect to MongoDB', err);
+        process.exit(1);
+    });
